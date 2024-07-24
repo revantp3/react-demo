4f8862b3a7a5c75c589e9540687e3a1f912331e1
jeyy10 <itsj10.jey@gmail.com>
2024-07-24 12:59:23 +0530

    design 404 page

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import { Figure, Image } from "react-bootstrap";
+import NotFoundImgPath from "../assets/images/404.svg";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404 Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+    <div className="d-flex justify-content-center align-items-center p-4 vh-100">
+      <Figure>
+        <Image src={NotFoundImgPath} alt="404 not found" fluid className="w-100" />
+      </Figure>
     </div>
   );
 };
